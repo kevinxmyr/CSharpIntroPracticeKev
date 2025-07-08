a3514828dbb21b74dc7919d6150c677ba4b99470
kevinxmyr <109541263+kevinxmyr@users.noreply.github.com>
2025-07-08 13:52:58 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyNamespace
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+
+         System.Console.WriteLine("enter an a value: ");
+         var a = double.Parse(Console.ReadLine());
+         System.Console.WriteLine("enter a b value: ");
+         // var b = !string.IsNullOrEmpty(Console.ReadLine()) ? double.Parse(Console.ReadLine()) : 0;
+         var bInput = Console.ReadLine();
+         var result = double.TryParse(bInput, out var b) ? b : 0;
+
+         System.Console.WriteLine($"value of a: {a}, value of b: {b},\n The result is: {result}");
+
+      }
+   }
+}
