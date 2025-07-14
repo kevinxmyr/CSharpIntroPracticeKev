009a5b0ddad511b28a5343ccc42355e90c0bdf3a
kevinxmyr <109541263+kevinxmyr@users.noreply.github.com>
2025-07-14 17:01:46 +0800

    FUNCTIONS AND PARAMETERS

--- obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0561ac09cdd6be7db4774e55f5aff718d6821327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3907ff26ce1d5310ffa34b7ea4464c9c58deb46")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
